03a3a6098cf9b53ffb9ec7220f4aa267c07f4ecc
Fabricio Godoy <skarllot@gmail.com>
2017-02-16 23:37:08 -0200

    Changes to build and pack for Net 3.5 for testing

--- build.bat
+++ build.bat
@@ -9,8 +9,8 @@ REM Cleanup output directory
 rmdir /s/q "%SolutionDir%Output" 2> nul
 mkdir "%SolutionDir%Output"
 
-REM CALL %SolutionDir%tools\build-wince.bat %SolutionDir% %SolutionName% %Project% || EXIT /B 1
 CALL %SolutionDir%tools\build-dotnet.bat %SolutionDir% %ProjectDotNet% net35-cf || EXIT /B 1
+CALL %SolutionDir%tools\build-dotnet.bat %SolutionDir% %ProjectDotNet% net35-client || EXIT /B 1
 
 echo build complete.
 echo.
